87888565b36d6fe9fd4694d18c71e49f3e685491
Wangels <kvack_kitan@hotmail.com>
2015-04-07 13:05:37 +0200

    removed the files we dont need from the lab5

--- app/js/searchCtrl.js
+++
@@ -1,8 +0,0 @@
-// Search controller that we use whenever we have a search inputs
-// and search results
-dinnerPlannerApp.controller('SearchCtrl', function ($scope,Dinner) {
-
-  // TODO in Lab 5: you will need to implement a method that searchers for dishes
-  // including the case while the search is still running.
-
-});

cf55dbdbceceb23b1af1d620409e949f5c16b077
SQUEEEE <squeeee@Zemeckis.(none)>
2015-04-11 12:49:00 +0200

    tried to use resource to access api, no success

--- app/js/homeCtrl.js
+++ app/js/homeCtrl.js
@@ -2,7 +2,20 @@
 iprogProj.controller('iprogCtrl', function ($scope, iprogProj) {
 
   	$scope.testVar = iprogProj.getVal();
+  	$scope.status = "nothing done"
 
-  	//$scope.testAPI = iprogProj.testAPI()
-	
+  	$scope.testAPI = function(){
+  		
+  		console.log("in testAPI")
+
+  		$scope.status = "requesting..."
+  		/*iprogProj.testAPI.get({}, function(data){
+  			console.log(data)
+  			$scope.status = "results"
+  		}, 
+  		function(data){
+	  		$scope.status = "There was an error";
+		});*/
+  	
+	}
 });
--- app/js/iprogModel.js
+++ app/js/iprogModel.js
@@ -6,6 +6,16 @@ iprogProj.factory('iprogProj',function ($resource) { //this had $cookieStore as 
 		return val
 	}
 
+	this.testAPI = $resource('https://api.ravelry.com/current_user.json', {}, {
+    	get: {
+			method: 'GET',
+			headers: { 
+				'Authorization': 'Basic RjlFOTc5ODBFMDc3MTA2Mjc3N0M6Rm9BeFVTekNFR2daS2dLTWxmZi04a0NWTnNqSFNRT1oxZGR3UlJxYw==',
+				'Access-Control-Allow-Origin': 'http://localhost:3000',
+			}
+		}
+	});
+
 	//super important don't forget
 	return this;
 })

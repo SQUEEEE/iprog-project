67275decc26a33c4a82acddaeef356c1f9278403
Axel <axehe@kth.se>
2015-05-13 14:50:04 +0200

    Filar på profile

--- app/js/profileCtrl.js
+++ app/js/profileCtrl.js
@@ -1,6 +1,14 @@
 iprogProj.controller('profileCtrl', function ($scope, iprogProj) {
 	$scope.getUser = iprogProj.getUser();
+	$scope.user = $scope.getUser.user;
+
 	$scope.picture = $scope.getUser.user.large_photo_url;
+	$scope.about_me = $scope.getUser.user.about_me;
+	$scope.location = $scope.getUser.user.location;
+	$scope.username = $scope.getUser.user.username;
+	$scope.first_name = $scope.getUser.user.first_name;
+	$scope.id = $scope.getUser.user.id;
+	
 
 
 })

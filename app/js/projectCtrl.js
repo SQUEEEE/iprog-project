66ad7c9326fbfa39421c7a9fa60c55baa36f4247
Molntussen <frida@molntussen.com>
2015-05-13 11:25:28 +0200

    Avatar o projektnamn finns med nu iaf....

--- app/js/projectCtrl.js
+++ app/js/projectCtrl.js
@@ -1,4 +1,10 @@
 iprogProj.controller('projectCtrl', function ($scope, iprogProj) {
 	$scope.getProject = iprogProj.getProject();
 	$scope.photos = $scope.getProject.project.photos;
+	$scope.projname = $scope.getProject.project.name;
+
+	$scope.getUser = iprogProj.getUser();
+	$scope.userName = $scope.getUser.user.username;
+	$scope.userPic = $scope.getUser.user.small_photo_url;
+
 })

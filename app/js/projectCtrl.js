8e107b2fc236066120ea2563ad4302ad7ae09aba
Wangels <kvack_kitan@hotmail.com>
2015-05-13 12:47:31 +0200

    trying to make the projectview nice

--- app/js/projectCtrl.js
+++ app/js/projectCtrl.js
@@ -1,4 +1,5 @@
 iprogProj.controller('projectCtrl', function ($scope, iprogProj) {
 	$scope.getProject = iprogProj.getProject();
 	$scope.photos = $scope.getProject.project.photos;
+	$scope.titel = $scope.getProject.project.name;
 })

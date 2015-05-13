67caca5df87f68025cac440ba61653c82261bc2d
Molntussen <frida@molntussen.com>
2015-05-13 14:37:21 +0200

    pattern petad på

--- app/js/patternCtrl.js
+++ app/js/patternCtrl.js
@@ -1,5 +1,14 @@
 iprogProj.controller('patternCtrl', function ($scope, iprogProj) {
 	$scope.getUser = iprogProj.getUser();
+	$scope.getPattern = iprogProj.getPattern();
 
+	$scope.photos = $scope.getPattern.pattern.photos;
+	
+	$scope.patternName = $scope.getPattern.pattern.name;
+
+	$scope.author = $scope.getPattern.pattern.pattern_author.name;
+	$scope.notes = $scope.getPattern.pattern.notes;
+	$scope.sizes = $scope.getPattern.pattern.sizes_available;
+	$scope.published = $scope.getPattern.pattern.published;
 
 })

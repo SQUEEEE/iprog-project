30bd6fd790f37e0f794a599797ff3af5632e1ba7
Wangels <kvack_kitan@hotmail.com>
2015-05-14 09:57:34 +0200

    did some cleaning up

--- app/js/profileCtrl.js
+++ app/js/profileCtrl.js
@@ -2,12 +2,12 @@ iprogProj.controller('profileCtrl', function ($scope, iprogProj) {
 	$scope.getUser = iprogProj.getUser();
 	$scope.user = $scope.getUser.user;
 
-	$scope.picture = $scope.getUser.user.large_photo_url;
-	$scope.about_me = $scope.getUser.user.about_me;
-	$scope.location = $scope.getUser.user.location;
-	$scope.username = $scope.getUser.user.username;
-	$scope.first_name = $scope.getUser.user.first_name;
-	$scope.id = $scope.getUser.user.id;
+	$scope.picture = $scope.user.large_photo_url;
+	$scope.about_me = $scope.user.about_me;
+	$scope.location = $scope.user.location;
+	$scope.username = $scope.user.username;
+	$scope.first_name = $scope.user.first_name;
+	$scope.id = $scope.user.id;
 	
 
 
